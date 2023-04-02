af5c4fc6b319dcfc108e485b8a9ed08f6bacbc84
cjuenger <christian@juenger.io>
2023-04-02 16:46:08 +0200

    feat: Add preparation to select product globally

---
+++ src/Io.Juenger.Scrum.Metrix.WebUI/Shared/Scrum/ProductSelection.razor.cs
@@ -0,0 +1,39 @@
+﻿using Io.Juenger.Scrum.GitLab.Contracts.Aggregates;
+using Io.Juenger.Scrum.GitLab.Contracts.Repositories;
+using Io.Juenger.Scrum.Metrix.WebUI.Configs;
+using Microsoft.AspNetCore.Components;
+
+namespace Io.Juenger.Scrum.Metrix.WebUI.Shared.Scrum;
+
+public partial class ProductSelection
+{
+    private IEnumerable<IProductAggregate>? _products;
+    private IProductAggregate? _selectedProduct;
+    
+    [Inject] 
+    private IProductRepository ProductRepository { get; set; } = default!;
+    
+    [Inject]
+    private ProductConfig ProductConfig { get; set; } = default!;
+    
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync().ConfigureAwait(false);
+        _products = await LoadProductsAsync().ConfigureAwait(false);
+    }
+    
+    private async Task<IEnumerable<IProductAggregate>> LoadProductsAsync()
+    {
+        var unorderedProducts = await ProductRepository
+            .LoadProductsAsync()
+            .ConfigureAwait(false);
+        
+        var orderedProducts = unorderedProducts.OrderBy(p => p.Name);
+        return orderedProducts;
+    }
+    
+    private async void OnSelectedProductChanged(object value)
+    {
+        await InvokeAsync(StateHasChanged).ConfigureAwait(false);
+    }
+}
